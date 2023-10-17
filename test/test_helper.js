3096bb5f4e9f143c0dd4b6d83a4b9b39fa242ebf
Wilson Juma Alcantara <wjuma.tech@gmail.com>
2023-10-16 21:08:20 -0400

    Enter func mongoose.connect into a mocha hook, before

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/users_test')
-mongoose.connection
-	.once('open', () => console.log('Connected succesfully!'))
-	.on('error', (err) => console.warn(err))
-
-
+before((done) => {
+	mongoose.connect('mongodb://localhost/users_test')
+	mongoose.connection
+		.once('open', () => done())
+		.on('error', (err) => console.warn(err))
+})
 
 beforeEach((done) => {
 	mongoose.connection.collections.users.drop((err, stream) => {
