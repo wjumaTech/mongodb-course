2fe4b42571984b3fab9ec9861e7b4e73f464079b
Wilson Juma Alcantara <wjuma.tech@gmail.com>
2023-10-17 21:30:52 -0400

    Can add subdocuments to an existing record

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -4,7 +4,36 @@ const User = require('../src/users.js')
 
 describe('Subdocuments', () => {
 
-	it('Can create a subdocument', () => {
+	it('Can create a subdocument', (done) => {
+		let joe = new User({ 
+			name: 'Joe',
+			posts: [
+				{
+					title: 'PostTitle'
+				}
+			]
+		})
+
+		joe.save()
+			.then(user => User.findOne({ name: 'Joe' }))
+			.then((user) => {
+				assert( user.posts[0]['title'] === 'PostTitle' )
+				done()
+			})
+	})
+
+	it('Can add subdocuments to an existing record', () => {
+
+		let joe = new User({ 
+			name: 'Joe',
+			posts: []
+		})
+
+		joe.save()
+			.then(user => {
+				console.log(user)
+			})
+
 		
 	})
-})
+}) 
